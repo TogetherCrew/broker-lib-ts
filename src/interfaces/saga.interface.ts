a9e9fe3bba90eaf681dc63b59c32af043ed5609e
Mohammad Twin <scientists.twin@gmail.com>
2023-05-18 13:40:53 +0400

    refactor: interfaces was refactored

---
+++ src/interfaces/saga.interface.ts
@@ -0,0 +1,16 @@
+import { IChoreography } from "./choreography.interface"
+import { Model } from "mongoose"
+
+export interface ISaga {
+    sagaId: string
+    choreography: IChoreography
+    status: string,
+    data: any
+    created_at: Date
+    next: () => void
+}
+
+export interface SagaModel extends Model<ISaga> {
+    paginate(filter: object, options: object): any;
+}
+  
